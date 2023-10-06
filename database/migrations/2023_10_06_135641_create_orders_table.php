f9a644a16749195008b7ef968ba0cbebfe91e2db
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-10-06 17:34:06 +0330

    update migration.

--- database/migrations/2023_10_06_135641_create_orders_table.php
+++ database/migrations/2023_10_06_135641_create_orders_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('product_id');
             $table->timestamps();
+            $table->softDelete();
         });
     }
 
